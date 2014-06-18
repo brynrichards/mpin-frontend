dd3c55bdc7d69b6d4ee1dcbed9be22cccfe4ff5d
Stanislav Mihaylov <stanislav.mihaylov@certivox.com>
2014-06-18 10:39:46 +0300

    Removed settings.json. Changed setInterval to setTimeout for getting the cache

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -105,7 +105,7 @@ var mpin = mpin || {};
 
         // Caching - monitor if new version of the cache exists
  
-        setInterval(function () { window.applicationCache.update(); }, 1000); // Check for an updated manifest file every 60 minutes. If it's updated, download a new cache as defined by the new manifest file.
+        setTimeout(function () { window.applicationCache.update(); }, 1000); // Check for an updated manifest file every 60 minutes. If it's updated, download a new cache as defined by the new manifest file.
  
         window.applicationCache.addEventListener('updateready', function(){ // when an updated cache is downloaded and ready to be used
                 window.applicationCache.swapCache(); //swap to the newest version of the cache
