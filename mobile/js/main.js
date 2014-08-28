216897a7fce7e175908f7eba6ef6b1be8f4af2be
Jordan Katsarov <jordna.katsarov@certivox.com>
2014-08-28 12:14:21 +0300

    logo here and home icon fixes

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -81,7 +81,7 @@ var mpin = mpin || {};
             identityCheckRegex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             setDeviceName: false
         },
-        touchevents: true
+        touchevents: false
     };
  
  
